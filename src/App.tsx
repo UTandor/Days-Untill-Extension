e2606ecac8d764bbe457c8d383bfaf9b23a770c1
UTandor <demonsouls34865@gmail.com>
2024-01-12 23:28:11 -0800

    Setup complete and everything works as intended

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  
+  return (
+    <div className="h-[300px] w-[300px] grid place-items-center">
+      <Button>Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
